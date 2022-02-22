8b76faf8a738676eab1feb20b77fd8fb7297496f
matthew-hocking <matthocking2439@gmail.com>
2022-02-23 12:09:41 +1300

    header complete - maps props init

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import GoogleMapsReact from 'google-maps-react'
+import { Paper, Typography, useMediaQuery } from '@material-ui/core'
+import locationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlinedIcon'
+import Rating from '@material-ui/lab'
+
+import useStyles from './styles'
 
 const Map = () => {
+
+  const classes = useStyles()
+  const isMobile = useMediaQuery('(min-width:600px')
+
+  const coordinates = { lat: 0, lng: 0}
+
   return (
-    <h1>Map</h1>
+    <div className={classes.mapContainer}>
+      <GoogleMapsReact
+        bootstrapURLKeys={{ key: 'AIzaSyCIEGbSauutGIt7yqtswsIoUJFySYYB9Us' }}
+        defaultCenter={coordinates}
+        center={coordinates}
+        defaultZoom={14}
+        margin={[ 50, 50, 50, 50 ]}
+        options={ '' }
+        onChange={ '' }
+        onChildClick={ '' }
+      >
+      </GoogleMapsReact>
+    </div>
   )
 }
 
